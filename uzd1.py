e80dd434740850fc8dbf1be30a02873736590939
emilsvagelis <emilvage@madona.edu.lv& C:/Users/skolens/AppData/Local/Programs/Python/Python310/python.exe c:/Users/skolens/Desktop/ProgrammesanaEmilsV/25nov/macas_2024/darbs.py>
2025-11-03 11:52:48 +0200

    Apskatīju pirmo uzd.

--- uzd1.py
+++ uzd1.py
@@ -1,7 +1,8 @@
+# sasummē ievadītos skaitļus
 summa = 0
 x = int(input("Ievadi skaitli (0 lai beigtu): "))
 while x != 0:
     summa += x
     x = int(input("Ievadi skaitli (0 lai beigtu): "))
 print("Summa:", summa)
-``
+
